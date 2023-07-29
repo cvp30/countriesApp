2453ef2955d025c149439395ceb69a6480f6808f
cvp30 <carlos.valer.p30@gmail.com>
2023-07-29 17:03:58 -0500

    update

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom"
 import SearchBar from "./SearchBar"
-import SideBar from "./SideBar"
+// import SideBar from "./SideBar"
 
 const MainPage = () => {
 
   return (
     <div className="w-full h-screen flex flex-col lg:flex-row justify-end items-center gap-4 lg:gap-0">
-      <SideBar />
+      {/* <SideBar /> */}
       <main className="flex-grow lg:h-full h-5/6 w-full">
         <SearchBar />
         <div className="w-full h-full lg:h-5/6 ">
